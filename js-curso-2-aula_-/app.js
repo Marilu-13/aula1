fff4f97a3ad5f053a071a5ecefb3e20830a78266
Maria Luz <filha.de.hades21@gmail.com>
2024-07-01 18:14:36 -0300

    Aula git1

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,72 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+
+let tentativa = 1;
+//
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, "Brazilian Portuguese Female", 
+        {rate:1.2});
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela("h1", "Bem vindo ao Jogo do Número Secreto");
+    exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+}
+
+exibirMensagemInicial();
+
+//
+function verificarChute() {
+    let chute = document.querySelector("input").value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela("h1", "Que tri, tu acertou!!");
+        let palavraTentativa = tentativa > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativa = `Tu acertou o número em ${tentativa} ${palavraTentativa}!`;
+        exibirTextoNaTela("p", mensagemTentativa);
+        document.getElementById("reiniciar").removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela("p", "Menos");
+            } else  {
+                exibirTextoNaTela("p", "Mais");
+    }
+    tentativa++;
+    limparcampo();
+}
+}
+//
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite +2);
+    let quantidadeDeElementosNaLsta = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLsta == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+ }
+//
+function limparcampo() {
+    chute = document.querySelector("input");
+    chute.value = "";
+}
+//
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparcampo();
+    tentativa = 1;
+    exibirMensagemInicial();
+    document.getElementById("reiniciar").setAttribute("disabled", true);
+}
+//
+
